a0627197e887609dac0bf2082050fb160ab4c609
Sudhir Kumar <2017ucp1296@mnit.ac.in>
2020-05-31 05:56:57 +0530

    Updates

---
+++ precache-manifest.dbcbc08049c5b6f97993957f982bdc1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3498408a45a8107cfb9c883330932d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba6dbbcedb38dcd227f",
+    "url": "/static/css/main.f66fdc77.chunk.css"
+  },
+  {
+    "revision": "49d0cbfb254a9b1d44a4",
+    "url": "/static/js/2.62cc62bc.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.62cc62bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba6dbbcedb38dcd227f",
+    "url": "/static/js/main.075cc041.chunk.js"
+  },
+  {
+    "revision": "9db62622f9583f71c350",
+    "url": "/static/js/runtime-main.010b8090.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);

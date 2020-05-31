becbfd21400708d4ac942897c05fde78e2ee57fb
Sudhir Kumar <2017ucp1296@mnit.ac.in>
2020-05-31 06:20:13 +0530

    Updates

---
+++ precache-manifest.af2316c4b5559c2da65b2e962f11da29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5806706ac8b442dec4d3f3ea5a1617",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ba6dbbcedb38dcd227f",
+    "url": "./static/css/main.f66fdc77.chunk.css"
+  },
+  {
+    "revision": "5b1cb071741d7fdea7cf",
+    "url": "./static/js/2.7a166e5f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "./static/js/2.7a166e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ba6dbbcedb38dcd227f",
+    "url": "./static/js/main.075cc041.chunk.js"
+  },
+  {
+    "revision": "9102353562f958011188",
+    "url": "./static/js/runtime-main.193089ff.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "./static/media/image.d7265326.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dbcbc08049c5b6f97993957f982bdc1c.js"
+  "./precache-manifest.af2316c4b5559c2da65b2e962f11da29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
